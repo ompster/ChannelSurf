fae1a97bca87ee54c54b6fffe89d36e888e47fae
Nathan Ash <nathan.ash@gmail.com>
2026-03-22 08:58:31 +1100

    Add PWA support - installable as app on mobile/TV/desktop

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'channelsurf-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network-first for everything (YouTube API needs network anyway)
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
