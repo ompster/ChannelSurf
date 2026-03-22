adae72cdff887617070b275d3b972916a9bea99a
Nathan Ash <nathan.ash@gmail.com>
2026-03-22 14:39:29 +1100

    Add retro synthwave logo (top-left overlay during playback)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'channelsurf-v1';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'channelsurf-v2';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo-sm.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
